9d881d0223ba2336301d5854978210f10f4f69e0
eleaga <lhbezerra@hotmail.com>
2015-09-04 17:45:56 -0300

    teste hello

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Gerenciador' });
 });
 
+router.get('/hello', function(req, res, next) {
+  res.render('hello', { title: 'Hello World' });
+});
+
 module.exports = router;
