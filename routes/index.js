f316f38cc7f9556afafa3b36ceff933d8b2330e2
eleaga <lhbezerra@hotmail.com>
2015-09-04 23:02:02 -0300

    teste hello

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var moments = require("../lib/data/momentsprovider-mongodb.js");
 var steps = require("../lib/data/stepprovider-mongodb.js");
+var scenarios = require("../lib/data/scenarioprovider-mongodb.js");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  steps.load(function(results){
+  moments.load(function(results){
     console.log(results);
   });
   res.render('index', { title: 'Gerenciador' });
@@ -18,7 +20,14 @@ router.get('export/behat', function(req, res, next) {
 });
 
 router.get('/hello', function(req, res, next) {
-  res.render('hello', { title: 'Hello World' });
+  
+  // moments.load(function(result){
+  // 	res.render('hello', { title: 'Hello World', moments: result } );
+  // });  
+
+  // moments.save();
+  // 
 });
+
 
 module.exports = router;
